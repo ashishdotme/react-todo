3a41c06fe030e501d53a435cfcfdf4e2695959e2
Ashish Patel <ashishsushilpatel@gmail.com>
2018-09-30 01:36:39 +0530

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import VisibleTodolist from '../containers/VisibleTodolist';
+import { Section } from 'react-bulma-components/full';
+const Main = ({ todos }) => (
+
+    <VisibleTodolist />
+
+);
+
+export default Main;
---
+++ src/containers/VisibleTodolist.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from '../actions'
+import Todolist from '../components/Todolist';
+
+const mapStateToProps = state => ({
+    todos: state.todos
+})
+
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+})
+
+const VisibleTodolist = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Todolist)
+
+export default VisibleTodolist
